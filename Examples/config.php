4627e98edad7c100f6700708272b8412ddc3f570
Vítězslav Dvořák <info@vitexsoftware.cz>
2017-03-01 03:03:22 +0100

    Test Connection Example Create Label Example Copy Invoice Example

---
+++ Examples/config.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * FlexiPeeHP - Example how to configure Connection
+ *
+ * @author     Vítězslav Dvořák <info@vitexsofware.cz>
+ * @copyright  (G) 2017 Vitex Software
+ */
+
+/**
+ * Write logs as:
+ */
+define('EASE_APPNAME', 'FlexiPeeHP-Example');
+
+/**
+ * Write logs TO: one of memory,console,file,syslog or combinations like "console|syslog"
+ */
+define('EASE_LOGGER', 'console');
+
+/*
+ * URL Flexibee API
+ */
+define('FLEXIBEE_URL', 'https://demo.flexibee.eu');
+/*
+ * FlexiBee API User Login
+ */
+define('FLEXIBEE_LOGIN', 'winstrom');
+/*
+ * FlexiBee API User Password
+ */
+define('FLEXIBEE_PASSWORD', 'winstrom');
+/*
+ * FlexiBee API Company
+ */
+define('FLEXIBEE_COMPANY', 'demo');
+
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # FlexiPeeHP-Examples
-Examples how to use FlexiPeeHP Library for FlexiBEE
+
+Examples how to use FlexiPeeHP Library for FlexiBee
+
+How to run ?
+------------
+
+1) composer update
+2) cd Examples
+3) modify config.php to use custom FlexiBee connection
+4) run the *.php files from commandline
+
