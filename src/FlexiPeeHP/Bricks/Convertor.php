5597bbf8e8cb18e006d0a29019699f86be57565e
Cybervitexus <info@vitexsoftware.cz>
2018-12-17 01:18:29 +0100

    Basic ability to make invoice from proforma

--- src/FlexiPeeHP/Bricks/Convertor.php
+++ src/FlexiPeeHP/Bricks/Convertor.php
@@ -37,10 +37,10 @@ class Convertor extends \Ease\Sand
      * Convertor 
      * 
      * @param \FlexiPeeHP\FlexiBeeRO $input
-     * @param \FlexiPeeHP\FlexiBeeRO $output
+     * @param \FlexiPeeHP\FlexiBeeRW $output
      */
     public function __construct(\FlexiPeeHP\FlexiBeeRO $input = null,
-                                \FlexiPeeHP\FlexiBeeRO $output = null)
+                                \FlexiPeeHP\FlexiBeeRW $output = null)
     {
         parent::__construct();
         if (!empty($input)) {
@@ -69,22 +69,46 @@ class Convertor extends \Ease\Sand
         $this->output = $destination;
     }
 
-    public function conversion()
+    public function conversion($keepId = false, $addExtId = true,
+                               $keepCode = false)
     {
-        $this->convertDocument();
+        $this->convertDocument($keepId, $addExtId, $keepCode);
         return $this->output;
     }
 
-    public function convertDocument()
+    static public function baseClassName($object)
     {
-        switch (get_class($this->input)) {
-            case 'FlexiPeeHP\Banka':
-                switch (get_class($this->output)) {
-                    case 'FlexiPeeHP\FakturaVydana':
+        return basename(str_replace('\\', '/', get_class($object)));
+    }
+
+    public function convertDocument($keepId = false, $addExtId = true,
+                                    $keepCode = false)
+    {
+        switch (self::baseClassName($this->input)) {
+            case 'FakturaVydana':
+            case 'Banka':
+                switch (self::baseClassName($this->output)) {
+                    case 'FakturaVydana':
                         // Banka['typPohybuK' => 'typPohybu.prijem'] -> FakturaVydana['typDokl' => 'ZDD']
 //                        $this->output->setDataValue('firma',  \FlexiPeeHP\FlexiBeeRO::code( $this->input->getDataValue('firma')));
-                        $this->rules = $this->commonItems();
+                        $this->rules = array_combine($this->commonItems(),
+                            $this->commonItems());
+                        unset($this->rules['stavUhrK']);
+                        unset($this->rules['datUcto']);
+                        unset($this->rules['datUcto']);
 
+
+                        foreach ($this->rules as $rule){
+                            if(preg_match('/^sum/', $rule)){
+                                unset($this->rules[$rule]);
+                            }
+                        }
+                        
+                        
+                        $polozkyDokladu                = new \FlexiPeeHP\FakturaVydanaPolozka();
+                        $itemColnames                  = array_keys($polozkyDokladu->getColumnsInfo());
+                        $this->rules['polozkyFaktury'] = self::removeRoColumns(array_combine($itemColnames,
+                                    $itemColnames), $polozkyDokladu);
 
                         break;
                     default :
@@ -98,11 +122,73 @@ class Convertor extends \Ease\Sand
                     get_class($this->input)));
                 break;
         }
+        $this->convertItems($keepId, $addExtId, $keepCode);
     }
 
-    public function convertItems()
+    public function convertItems($keepId = false, $addExtId = true,
+                                 $keepCode = false)
     {
+        if ($keepCode === false) {
+            unset($this->rules['kod']);
+        }
+        if ($keepId === false) {
+            unset($this->rules['id']);
+        }
+        foreach (self::removeRoColumns($this->rules, $this->output) as $columnToTake => $subitemColumns) {
         
+            if (is_array($subitemColumns)) {
+
+                if (self::isAssoc($this->input->data[$columnToTake])) {
+                    $sourceData = [$this->input->data[$columnToTake]];
+                } else {
+                    $sourceData = $this->input->data[$columnToTake];
+    }
+
+                foreach ($sourceData as $subitemData) {
+                    foreach ($subitemColumns as $subitemColumn) {
+                        if (!array_key_exists($subitemColumn, $subitemColumns)) {
+                            unset($subitemData[$subitemColumn]);
+                        }
+                    }
+
+                    if ($keepCode === false) {
+                        unset($subitemData['kod']);
+                    }
+                    if ($keepId === false) {
+                        unset($subitemData['id']);
+                    }
+
+
+                    $this->output->addArrayToBranch($subitemData);
+                }
+            } else {
+
+                $this->output->setDataValue($columnToTake,
+                    $this->input->getDataValue($columnToTake));
+            }
+        }
+        if ($addExtId) {
+            $this->output->setDataValue('id',
+                'ext:src:'.$this->input->getEvidence().':'.$this->input->getMyKey());
+        }
+    }
+
+    /**
+     * Returns only writable columns
+     * 
+     * @param array                  $rules   all Columns
+     * @param \FlexiPeeHP\FlexiBeeRW $engine  saver class
+     */
+    public static function removeRoColumns(array $rules, $engine)
+    {
+
+        foreach ($rules as $column) {
+            $columnInfo = $engine->getColumnInfo($column);
+            if ($columnInfo['isWritable'] == 'false') {
+                unset($rules[$column]);
+            }
+        }
+        return $rules;
     }
 
     /**
