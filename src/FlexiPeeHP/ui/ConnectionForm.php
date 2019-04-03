c99e133c1b36ca8189505273d66313f292cc5472
Cybervitexus <info@vitexsoftware.cz>
2019-04-04 01:27:22 +0200

    User Name column name fix

--- src/FlexiPeeHP/ui/ConnectionForm.php
+++ src/FlexiPeeHP/ui/ConnectionForm.php
@@ -24,7 +24,7 @@ class ConnectionForm extends \Ease\TWB\Form
      * FlexiBee User Input name
      * @var string eg. winstrom
      */
-    public $usernameField = 'login';
+    public $usernameField = 'user';
 
     /**
      * FlexiBee Password Input name
